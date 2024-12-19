f2c0b452fc1d72e9b10785bca70b014499a57414
khan <umar1098765@gmail.com>
2024-12-19 15:45:53 +0500

    vazfda6

---
+++ autos/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='cars')
+    photo = models.ImageField(upload_to='car_photos/')
+
+    def __str__(self):
+        return self.name
